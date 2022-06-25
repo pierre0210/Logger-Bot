a5958fd825e85005e57c9d61db50f3bd9e6fc1c1
pierre0210 <conandoyle.0210@gmail.com>
2022-06-25 15:26:19 +0800

    Add project files.

---
+++ Logger/Program.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace Logger
+{
+    public class Program
+    {
+        private static DiscordSocketClient _client;
+        static void Main(string[] args)
+        {
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
