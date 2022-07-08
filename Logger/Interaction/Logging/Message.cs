80205766df5d36e546b21e9ef49d221ad6d59a65
pierre0210 <conandoyle.0210@gmail.com>
2022-07-08 21:57:25 +0800

    add message logging system basic structure

---
+++ Logger/Interaction/Logging/Message.cs
@@ -0,0 +1,41 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Logger.Database;
+using Logger.Database.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger.Interaction.Logging
+{
+    [DefaultMemberPermissions(GuildPermission.Administrator)]
+    [Group("message-log", "message logging")]
+    public class Message
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly IServiceProvider _service;
+        private readonly InteractionService _interaction;
+
+        public Message(DiscordSocketClient client, IServiceProvider service, InteractionService interaction)
+        {
+            _client = client;
+            _service = service;
+            _interaction = interaction;
+        }
+
+        [SlashCommand("enable", "enable message logging")]
+        public async Task EnableAsync()
+        {
+
+        }
+
+        [SlashCommand("disable", "disable message logging")]
+        public async Task DisableAsync()
+        {
+
+        }
+    }
+}
