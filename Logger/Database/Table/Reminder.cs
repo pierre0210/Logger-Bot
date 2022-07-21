a32f9432c96f459eb5af1948d0a22a95b1d96363
pierre0210 <conandoyle.0210@gmail.com>
2022-07-21 23:06:26 +0800

    add reminder table

---
+++ Logger/Database/Table/Reminder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger.Database.Table
+{
+    public class Reminder : DbEntity
+    {
+        public ulong UserId { get; set; }
+        public string TimeStamp { get; set; } = String.Empty;
+        public int Duration { get; set; }
+        public string Content { get; set; } = String.Empty;
+    }
+}
---
+++ Logger/Interaction/Reminder/Reminder.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Logger;
+using Logger.Database;
+using Logger.Database.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger.Interaction.Reminder
+{
+    [Group("reminder", "reminder command")]
+    public class Reminder
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly IServiceProvider _service;
+        private readonly InteractionService _interaction;
+
+        public Reminder(DiscordSocketClient client, IServiceProvider service, InteractionService interaction)
+        {
+            _client = client;
+            _service = service;
+            _interaction = interaction;
+        }
+
+        [SlashCommand("add", "add new reminder")]
+        public async Task AddAsync([Summary(description: "小時")]int hours, [Summary(description: "分鐘")]int minutes, string content)
+        {
+            RedisUtility utility = new RedisUtility(Program.RedisDb);
+        }
+    }
+}
